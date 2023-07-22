11380806be412dc4229e8ea78a51fba10c4a9f0b
cpneppal <cpneppal@buffalo.edu>
2023-07-21 21:20:29 -0400

    Tinkered with the paths and got file IO to work

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -5,21 +5,17 @@ mod route;
 pub use request::DeconstructedHTTPRequest;
 pub use response::{HTTPResponses, Response, Result};
 pub use route::Router;
+use std::fs;
 pub use HTTPResponses::*;
-
 pub fn http_routes() -> Router {
     Router::new()
         .route("GET", r"/(\d+)$", "1.1", |_, _| Ok("Hello, World!".into()))
         .and_then(|r| {
-            r.route("POST", r"/(\d+)$", "1.1", |_, body| {
-                println!("Body => {body:?}");
-                Ok(Redirect("/".to_owned()))
-            })
-        })
-        .and_then(|r| {
             r.route("POST", r"/$", "1.1", |_, body| {
-                println!("Body => {body:?}");
-                Ok(Redirect("/".to_owned()))
+                //println!("Body => {body:?}");
+                fs::write("result.jpeg", body)
+                    .map_err(|_| HTTPResponses::internal_server_error())?;
+                Ok("Succeeded in writing image!".into())
             })
         })
         .unwrap()
