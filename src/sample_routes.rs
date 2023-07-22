3bc2fe23e6a8ee2a119d04218080dc34bae5d9cd
cpneppal <cpneppal@buffalo.edu>
2023-07-22 16:48:33 -0400

    Added some more internal routes

--- src/sample_routes.rs
+++ src/sample_routes.rs
@@ -1,8 +1,12 @@
-use http::{HTTPRequest, HTTPResponses, Result, Router};
+use http::{
+    HTTPRequest,
+    HTTPResponses::{self, *},
+    Result, Router,
+};
 
 pub fn http_routes() -> Router {
     Router::new()
-        .route("GET", "/$", "1.1", hello_world)
+        .route("GET|POST", "/$", "1.1", hello_world)
         .and_then(|route| route.route("POST", "/image$", "1.1", get_image))
         .and_then(|route| route.route("POST", "/user_json$", "1.1", print_json))
         .unwrap()
@@ -14,7 +18,7 @@ fn hello_world(_headers: HTTPRequest, _body: Vec<u8>) -> Result<HTTPResponses> {
 
 fn get_image(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
     println!("Body Length: {}", body.len());
-    Ok("Image Received!".into())
+    Ok(Redirect("/".to_owned()))
 }
 
 fn print_json(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
@@ -25,5 +29,9 @@ fn print_json(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
             HTTPResponses::internal_server_error()
         })?
     );
-    Ok("Received Json!".into())
+    Err(HTTPError {
+        status_code: 500,
+        message: "Some Server Error".to_owned(),
+        body: "Congrats, you've broken our site!".to_owned(),
+    })
 }
