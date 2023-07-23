6c7c34f7f7020a7bff2300c4cc0fa512adbd25a2
cpneppal <cpneppal@buffalo.edu>
2023-07-23 16:51:10 -0400

    Changed and tinkered with the sample routes to test previous commit.
    Please consult the example here for some documentation on how the callback
    functions should be constructed.

--- src/sample_routes.rs
+++ src/sample_routes.rs
@@ -1,27 +1,38 @@
 use http::{
-    HTTPRequest,
+    http_err, http_ok, HTTPRequest,
     HTTPResponses::{self, *},
-    Result, Router,
+    HTTPResult, Router,
 };
-
 pub fn http_routes() -> Router {
     Router::new()
         .route("GET|POST", "/$", "1.1", hello_world)
         .and_then(|route| route.route("POST", "/image$", "1.1", get_image))
         .and_then(|route| route.route("POST", "/user_json$", "1.1", print_json))
+        .and_then(|route| route.route("GET|POST", "/custom$", "1.1", custom_route))
         .unwrap()
 }
 
-fn hello_world(_headers: HTTPRequest, _body: Vec<u8>) -> Result<HTTPResponses> {
-    Ok("Hello, world!".into())
+fn hello_world(_headers: HTTPRequest, _body: Vec<u8>) -> HTTPResult {
+    http_ok("Hello, world!".into())
 }
 
-fn get_image(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
+fn custom_route(headers: HTTPRequest, _: Vec<u8>) -> HTTPResult {
+    println!("Headers => {headers:?}");
+    http_ok(Custom {
+        code: 201,
+        message: "Created".to_owned(),
+        ctype: "text/plain".to_owned(),
+        headers: None,
+        body: Vec::from("Something has occured!".as_bytes()),
+    })
+}
+
+fn get_image(_headers: HTTPRequest, body: Vec<u8>) -> HTTPResult {
     println!("Body Length: {}", body.len());
-    Ok(Redirect("/".to_owned()))
+    http_ok(Redirect("/".to_owned()))
 }
 
-fn print_json(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
+fn print_json(_headers: HTTPRequest, body: Vec<u8>) -> HTTPResult {
     println!(
         "Json Receieved: {}",
         String::from_utf8(body).map_err(|err| {
@@ -29,7 +40,7 @@ fn print_json(_headers: HTTPRequest, body: Vec<u8>) -> Result<HTTPResponses> {
             HTTPResponses::internal_server_error()
         })?
     );
-    Err(HTTPError {
+    http_err(HTTPError {
         status_code: 500,
         message: "Some Server Error".to_owned(),
         body: "Congrats, you've broken our site!".to_owned(),
